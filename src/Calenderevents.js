a3ffeb97ece32e8c9a505d125062b1d88198eb70
Himanshu Patil <patilhnofficial@gmail.com>
2022-06-11 00:33:09 +0530

    update

--- src/Calenderevents.js
+++ src/Calenderevents.js
@@ -4,19 +4,26 @@ const Calenderevents = ({ events }) => {
   return (
     <div className="m-20">
       <table>
-        <tr className="border">
-          <th className="border text-center">Summary</th>
-          <th className="border text-center">Creator</th>
-          <th className="border text-center">Status</th>
-          <th className="border text-center">Link</th>
+        <tr className="bg-blue-200 rounded-t">
+          <th className="h-14 border text-center rounded-tl">Summary</th>
+          <th className="h-14 border text-center">Creator</th>
+          <th className="h-14 border text-center">Status</th>
+          <th className="h-14 border text-center rounded-tr">Link</th>
         </tr>
         {events?.map((meeting) => (
           <tr key={meeting.id}>
-            <td className="border text-center">{meeting.summary}</td>
-            <td className="border text-center">{meeting.creator?.email}</td>
-            <td className="border text-center">{meeting.status}</td>
-            <td className="border text-center">
-              <a target="_blank" rel="noreferrer" href={meeting.htmlLink}>
+            <td className=" p-4 border text-center">{meeting.summary}</td>
+            <td className=" p-4 border text-center">
+              {meeting.creator?.email}
+            </td>
+            <td className=" p-4 border text-center">{meeting.status}</td>
+            <td className=" p-4 border text-center">
+              <a
+                className="text-blue-700"
+                target="_blank"
+                rel="noreferrer"
+                href={meeting.htmlLink}
+              >
                 {meeting.htmlLink}
               </a>
             </td>
